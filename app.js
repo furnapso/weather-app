707f32c90d33422f340cc71cc82c06c0112709d1
furnapso <luke.boland95@gmail.com>
2023-01-06 12:13:03 +1000

    finish app

--- app.js
+++ app.js
@@ -1,20 +1,76 @@
-const apiUrl = "http://api.openweathermap.org"
-const geoApiPath = "/geo/1.0/direct?q="
-const weatherApiPath = "/data/3.0/onecall?"
-const apiKey = "bf4cd5466179067780d1737f57afdbd1"
+const weatherCodes = {
+    0: 'Clear Sky',
+    1: 'Mainly clear',
+    2: 'Partly cloudy',
+    3: 'Overcast',
+    45: 'Fog',
+    48: 'Depositing rime fog',
+    51: 'Light drizzle',
+    53: 'Moderate drizzle',
+    55: 'Dense drizzle',
+    56: 'Light freezing drizzle',
+    57: 'Dense freezing drizzle',
+    61: 'Slight rain',
+    63: 'Moderate rain',
+    65: 'Heavy rain',
+    66: 'Light freezing rain',
+    67: 'Heavy freezing rain',
+    71: 'Light snow',
+    73: 'Moderate snow',
+    75: 'Heavy snow',
+    77: 'Snow grains',
+    80: 'Slight showers',
+    81: 'Moderate showers',
+    82: 'Violent showers',
+    85: 'Slight snow showers',
+    86: 'Heavy snow showers',
+    95: 'Thunderstorm',
+    96: 'Thunderstorm with slight hail',
+    99: 'Thunderstorm with heavy hail'
+}
 
 async function getLatLong(cityName) {
-    return await fetch(`${apiUrl}${geoApiPath}${cityName}&limit=1&appid=${apiKey}`)
+    return await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`)
         .then(r => r.json())
         .then(d => {
             return {
-                'lat': d[0].lat,
-                'long': d[0].lon
+                lat: d.results[0].latitude,
+                lon: d.results[0].longitude,
+                country: d.results[0].country
             }
         })
 }
 
 async function getWeatherData(cityName) {
     const { lat, lon } = await getLatLong(cityName)
-    return await fetch(`${apiUrl}${weatherApiPath}lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    return await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`)
+        .then(r => r.json())
 }
+
+async function updateWeather() {
+    displayLoading()
+    const formData = new FormData(document.querySelector("form"))
+    const cityName = formData.get("city")
+    if (cityName != null && cityName.trim() != "") {
+        console.log('cityName:', cityName)
+        const weatherData = await getWeatherData(cityName)
+        console.log(weatherData)
+
+        let weatherElement = `
+        <h2>${cityName}</h2>
+        <h3>${weatherData.current_weather.temperature}°c</h3>
+        <h4>${weatherCodes[weatherData.current_weather.weathercode]}</h4>
+        `
+        document.querySelector("#weatherContainer").innerHTML = weatherElement
+    }
+}
+
+function displayLoading() {
+    const loading = `<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`
+    document.querySelector("#weatherContainer").innerHTML = loading
+}
+
+document.querySelector("button").addEventListener('click', updateWeather)
+document.querySelector("form").addEventListener('click', e => {
+    e.preventDefault()
+})
